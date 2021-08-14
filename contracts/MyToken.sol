b3e05fd2b91c715aee1ea51c11040f9ccbdf5293
Indrajith V <indrajithvgp@yahoo.com>
2021-08-14 12:52:00 +0530

    token contract added

---
+++ contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Chakra", "CKR"){
+        _mint(msg.sender, initialSupply);
+    }
+
+    // constructor(uint256 initialSupply) ERC20("Chakra", "CKR") public{
+    //     _mint(msg.sender, initialSupply);
+    // }
+}
