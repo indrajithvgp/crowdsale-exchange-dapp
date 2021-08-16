c31f35ea82e253ba3de063402aa464a7c771d647
Indrajith V <indrajithvgp@yahoo.com>
2021-08-16 14:25:46 +0530

    kyc contract added for token purchase

--- contracts/MyTokenSale.sol
+++ contracts/MyTokenSale.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.0;
 
 import "./Crowdsale.sol";
+import "./KycContract.sol";
 
 contract MyTokensale is Crowdsale{
+    KycContract kyc;
     constructor(
         uint256 rate,    // rate in TKNbits
         address payable wallet,
-        IERC20 token
+        IERC20 token, 
+        KycContract _kyc
     )
         Crowdsale(rate, wallet, token)
         public
     {
+        kyc = _kyc
+    }
 
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view {
+     super._preValidatePurchase(beneficiary, weiAmount)
+     require(kyc.kycCompleted(msg.sender), "KYC not completed, no purchase allowed");
+    
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 let MyToken = artifacts.require("./MyToken.sol");
-let MyTokensale = artifacts.require("./MyTokensale.sol");
+// let MyTokensale = artifacts.require("./MyTokensale.sol");
 
 module.exports = async function(deployer) {
   let address = web3.eth.getAccounts()
-  await deployer.deploy(MyToken, 100);
-  await deployer.deploy(MyTokensale, 1, address[0], MyToken.address);
-  let instance = await MyToken.deployed()
-  await instance.transfer(MyTokensale, 100)
+  await deployer.deploy(MyToken, 1000000);
+  // await deployer.deploy(MyTokensale, 1, address[0], MyToken.address);
+  // let instance = await MyToken.deployed()
+  // await instance.transfer(MyTokensale, 100)
 
 };
